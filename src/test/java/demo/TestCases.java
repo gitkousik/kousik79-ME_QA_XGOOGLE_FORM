4f1fa11afb657f460268f9e12449318cce536c81
gitkousik <kousik79@gmail.com>
2026-09-19 13:26:04 +0530

    ME_QA_XGOOGLE_FORM_MODULE_L1

--- src/test/java/demo/TestCases.java
+++ src/test/java/demo/TestCases.java
@@ -13,6 +13,7 @@ import org.testng.annotations.Test;
 import java.util.logging.Level;
 // import io.github.bonigarcia.wdm.WebDriverManager;
 import demo.wrappers.Wrappers;
+import org.testng.Assert;
 
 public class TestCases {
     ChromeDriver driver;
@@ -21,6 +22,71 @@ public class TestCases {
      * TODO: Write your tests here with testng @Test annotation. 
      * Follow `testCase01` `testCase02`... format or what is provided in instructions
      */
+    @Test
+    public void testCase01(){
+        //Navigate to google form
+        Wrappers.navigateTo(driver,"https://docs.google.com/forms/d/e/1FAIpQLSep9LTMntH5YqIXa5nkiPKSs283kdwitBBhXWyZdAS-e4CxBQ/viewform");
+
+        //Fill in Crio Learner in the 1st text box
+        By nameField = By.xpath("//div[@role='listitem'][1]//input[@type='text']");
+        Wrappers.enterText(driver, nameField, "Crio Learner");
+
+        //Write down "I want to be the best QA Engineer! 1710572021'' where 1710572021 is variable - needs to be the current epoch time.
+        By reasonField = By.xpath("//div[@role='listitem'][2]//textarea");
+        String epochTime = String.valueOf(
+        java.time.Instant.now().getEpochSecond());
+        String reason = "I want to be the best QA Engineer! " + epochTime;
+        Wrappers.enterText(driver, reasonField, reason);
+
+        //Enter your Automation Testing experience in the next radio button
+        By experienceRadio = By.cssSelector("div[role='radio'][data-value='3 - 5']");
+        Wrappers.clickElement(driver, experienceRadio);
+
+        //Select Java, Selenium and TestNG from the next check-box
+        By javaCheckbox = By.cssSelector("div[role='checkbox'][data-answer-value='Java']");
+        By seleniumCheckbox = By.cssSelector("div[role='checkbox'][data-answer-value='Selenium']");
+        By testngCheckbox = By.cssSelector("div[role='checkbox'][data-answer-value='TestNG']");
+        Wrappers.clickElement(driver, javaCheckbox);
+        Wrappers.clickElement(driver, seleniumCheckbox);
+        Wrappers.clickElement(driver, testngCheckbox);
+
+        //Provide how you would like to be addressed in the next dropdown
+        By addressDropdown = By.cssSelector("div[role='listbox']");
+        By mrOption = By.cssSelector("div[role='option'][data-value='Mr']");
+        Wrappers.selectFromDropdown(driver, addressDropdown, mrOption);
+
+        //Provided the current date minus 7 days in the next date field, it should be dynamically calculated and not hardcoded.
+        By dateField = By.xpath("//div[@role='listitem'][.//div[contains(normalize-space(), 'What was the date 7 days ago?')]]//input[@type='date']");
+        String dateSevenDaysAgo = java.time.LocalDate.now()
+            .minusDays(7)
+            .toString();
+
+        Wrappers.enterDate(driver, dateField, dateSevenDaysAgo);
+
+        //Provide the time 07:30 in the next field (Can also be in 24 hour clock)
+        By hourField = By.cssSelector("input[aria-label='Hour']");
+        By minuteField = By.cssSelector("input[aria-label='Minute']");
+
+        Wrappers.enterText(driver, hourField, "07");
+        Wrappers.enterText(driver, minuteField, "30");
+
+        //Submit the form
+        By submitButton = By.cssSelector("div[role='button'][aria-label='Submit']");
+        Wrappers.clickElement(driver, submitButton);
+
+        System.out.println("Form submitted successfully.");
+
+        //You will see a success message on the website. Print the same message on the console upon successful completion
+        By successMessage = By.xpath("//div[normalize-space()='Thanks for your response, Automation Wizard!']");
+        
+        if (Wrappers.isElementDisplayed(driver, successMessage)) {
+            String message = Wrappers.getText(driver, successMessage);
+            System.out.println(message);
+        } else {
+            System.out.println("ERROR: Form submission was not successful.");
+            Assert.fail("Form submission failed - success message was not displayed.");
+        }
+    }
 
      
     /*
--- src/test/java/demo/wrappers/Wrappers.java
+++ src/test/java/demo/wrappers/Wrappers.java
@@ -1,19 +1,89 @@
 package demo.wrappers;
 
-import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import java.time.Duration;
 
-import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import java.time.Duration;
+import org.openqa.selenium.JavascriptExecutor;
 
 public class Wrappers {
-    /*
-     * Write your selenium wrappers here
-     */
+
+    // Wrapper for navigating to a URL
+    public static void navigateTo(ChromeDriver driver, String url) {
+        driver.get(url);
+    }
+
+    // Wrapper for getting text from an element
+    public static String getText(ChromeDriver driver, By locator) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement element = wait.until(
+            ExpectedConditions.visibilityOfElementLocated(locator)
+        );
+        return element.getText();
+    }
+
+    //Wrapper for wating for an element before interacting with it
+    public static void waitForElement(ChromeDriver driver, By locator) 
+    {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+
+    //Wrapper for entering text in text box element
+    public static void enterText(ChromeDriver driver, By locator, String text) 
+    {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement element = wait.until(ExpectedConditions.elementToBeClickable(locator));
+        element.click();
+        element.sendKeys(text);
+    }
+
+    //Wrapper for clicking on an element
+    public static void clickElement(ChromeDriver driver, By locator) 
+    {
+        waitForElement(driver, locator);
+        WebElement element = driver.findElement(locator);
+        element.click();
+    }
+
+    //Wrapper for dropdown selection
+    public static void selectFromDropdown(ChromeDriver driver, By dropdownLocator,By optionLocator) 
+    {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement dropdown = wait.until(ExpectedConditions.elementToBeClickable(dropdownLocator));
+        dropdown.click();
+        WebElement option = wait.until(ExpectedConditions.elementToBeClickable(optionLocator));
+        option.click();
+    }
+
+    //Wrapper for entering date
+    public static void enterDate(ChromeDriver driver,By locator,String dateValue) 
+    {
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement element = wait.until(ExpectedConditions.elementToBeClickable(locator));
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript(
+            "arguments[0].value = arguments[1];" +
+            "arguments[0].dispatchEvent(new Event('input', {bubbles:true}));" +
+            "arguments[0].dispatchEvent(new Event('change', {bubbles:true}));",
+            element,
+            dateValue
+        );
+    }
+
+     //Wrapper for checking elemnet displyed or not
+    public static boolean isElementDisplayed(ChromeDriver driver, By locator) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        try {
+            return wait.until(
+                ExpectedConditions.visibilityOfElementLocated(locator)
+            ).isDisplayed();
+        } catch (Exception e) {
+            return false;
+        }
+    }
 }
